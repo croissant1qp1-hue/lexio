644cd71c06517eb3a1418db96c88de0bd3bacc8e
glove <theodiesch@gmail.com>
2026-08-02 15:07:52 +0000

    basics für js layot bei vokabeln-hinzufügen

--- Frontend/script/vokabeln-hinzufügen/main-script-hinzufügen-seite.js
+++ Frontend/script/vokabeln-hinzufügen/main-script-hinzufügen-seite.js
@@ -1,3 +1,40 @@
-const buttonKarteikarten = document.getElementById("karteikarten");
-buttonKarteikarten.classList.add("active");
-alert("test")
+const franzButton = document.getElementById("französisch-button");
+const italienButton = document.getElementById("italienisch-button");
+const spanischButton = document.getElementById("spanisch-button");
+const englischButton = document.getElementById("englisch-button");
+const mainBody = document.getElementById("main-body")
+const link = document.createElement("link");
+
+link.rel = "stylesheet";
+link.href = "/Frontend/styles/vokabeln-hinzufügen-seite/vokabeln-hinzufügen-input-seite.css";
+
+const sprachenFarben = {
+    englisch: "#df9b35",
+    französisch: "#801e2e",
+    spanisch: "#c85d43",
+    italienisch: "#5c7d64"
+};
+
+let hintergrundFarbe = "#fff";
+
+function htmlAnpass(sprache) {
+    hintergrundFarbe = sprachenFarben[sprache];
+    alert(hintergrundFarbe);
+    document.head.appendChild(link);
+};
+
+function spracheAuswahl() {
+    franzButton.addEventListener("click", function() {
+        htmlAnpass("französisch")
+    });
+    italienButton.addEventListener("click", function() {
+        htmlAnpass("italienisch")
+    });
+    spanischButton.addEventListener("click", function() {
+        htmlAnpass("spanisch")
+    });
+    englischButton.addEventListener("click", function() {
+        htmlAnpass("englisch")
+    });
+};
+spracheAuswahl();
