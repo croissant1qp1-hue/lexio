e84b29e89ec6567c5d802cd53dad110a13eb9312
glove <theodiesch@gmail.com>
2026-08-02 16:55:59 +0000

    noch nicht viel design aber basic javascript für die vokabeln hinzufügen hinzugefügt

--- Frontend/script/vokabeln-hinzufügen/main-script-hinzufügen-seite.js
+++ Frontend/script/vokabeln-hinzufügen/main-script-hinzufügen-seite.js
@@ -1,13 +1,45 @@
+//Die vier knöpfe auf der anfang seite zum sprache auswählen
 const franzButton = document.getElementById("französisch-button");
 const italienButton = document.getElementById("italienisch-button");
 const spanischButton = document.getElementById("spanisch-button");
 const englischButton = document.getElementById("englisch-button");
+//saxhen wie der main-body also der screen
 const mainBody = document.getElementById("main-body")
-const link = document.createElement("link");
+//hier kommen alle links rein
+const linkZuStyleNeuFürInput = document.createElement("link");
+//jetzt für die vokabeln hinzufügen seite
+const vokabelnHinzufügenSeite = document.getElementById("vokabeln-hinzufügen-seite");
+//sprache auswählen seite
+const spracheAuswählenSeite = document.getElementById("seite-sprache-auswahl");
+const inputDeutsch = document.getElementById("deutsch-input-id");
+const inputFremd = document.getElementById("fremd-input-id");
 
-link.rel = "stylesheet";
-link.href = "/Frontend/styles/vokabeln-hinzufügen-seite/vokabeln-hinzufügen-input-seite.css";
+vokabelnHinzufügenSeite.style.display = "none";
+spracheAuswählenSeite.style.display = "block";
 
+const placeholderTexte = {
+    englisch: {
+        deutsch: "Katze",
+        fremd: "cat",
+    },
+
+    französisch: {
+        deutsch: "Garten",
+        fremd: "jardin",
+    },
+
+    spanisch: {
+        deutsch: "Hallo",
+        fremd: "hola",
+    },
+
+    italienisch: {
+        deutsch: "Apfel",
+        fremd: "mela",
+    }
+};
+
+//das ist eine liste mit den farben für die verschiedenen sprachen
 const sprachenFarben = {
     englisch: "#df9b35",
     französisch: "#801e2e",
@@ -17,12 +49,24 @@ const sprachenFarben = {
 
 let hintergrundFarbe = "#fff";
 
+//die funktion sorgt dafür das ein neuer link verlingt wird
+function dateiHinzufügen(sprache,link,konstante) {
+    konstante.rel = sprache;
+    konstante.href = link
+    document.head.appendChild(konstante)
+}
+//das ist sozusagen der zweite schritt weil das passt die farben und auch so sachen wie placeholder an anhand der ausgewählten sprache
 function htmlAnpass(sprache) {
     hintergrundFarbe = sprachenFarben[sprache];
-    alert(hintergrundFarbe);
-    document.head.appendChild(link);
-};
+    dateiHinzufügen("stylesheet","/Frontend/styles/vokabeln-hinzufügen-seite/vokabeln-input-seite.css",linkZuStyleNeuFürInput);
+    vokabelnHinzufügenSeite.style.display = "block";
+    spracheAuswählenSeite.style.display = "none";
+    mainBody.style.background = hintergrundFarbe;
+    inputDeutsch.placeholder = placeholderTexte[sprache].deutsch;
+    inputFremd.placeholder = placeholderTexte[sprache].fremd;
 
+};
+//das chekt welcher button  geklickt wird und gibt das halt weiter in die funktion html anpass
 function spracheAuswahl() {
     franzButton.addEventListener("click", function() {
         htmlAnpass("französisch")
