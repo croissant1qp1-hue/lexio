3637abbb3cea7a602c3a56a7034b66317e3911cd
glove <theodiesch@gmail.com>
2026-09-06 23:41:16 +0200

    chore: import global and navbar CSS in RootLayout, add FontAwesome link, fix RootLayout typing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+import "@/app/global.css";
+import "@/components/navbar/side-bar.css";
 import Navbar from "@/components/navbar/navbar";
 
-
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="de">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
+      </head>
       <body>
         <div className="container">
           <Navbar />
