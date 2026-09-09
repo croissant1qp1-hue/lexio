c3b7062f28451e832a52a1cd14e3f5f3f14a2236
glove <theodiesch@gmail.com>
2026-09-09 14:35:07 +0000

    hinzufügen seite fertig

---
+++ app/karteikarten-hinzufuegen/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import styles from "@/app/styles/modules/vokabeln-hinzufügen/vokabeln-hinzufügen.module.css"
+
+export default function SeiteSpracheAuswahl() {
+    return (
+        <div className={styles.mainBody}>
+            <div className={styles.seiteSpracheAuswahl} id="seite-sprache-auswahl">
+                <div className={styles.überschriftDiv}>
+                    <h3 className={styles.überschrift}>Welche Sprache möchtest du lernen</h3>
+                </div>
+                <div className={styles.sprachenAuswahl}>
+                    <div className={styles.row}>
+                        <Image src="/images/vokabel-karten-hinzufügen/sprache-auswählen/englisch.svg" className={styles.spracheAuswählenImg} alt="englisch-card" id="englisch-button" width={412} height={412} />
+                        <Image src="/images/vokabel-karten-hinzufügen/sprache-auswählen/französisch.svg" className={styles.spracheAuswählenImg} alt="franz-card" id="französisch-button" width={412} height={412} />
+                    </div>
+                    <div className={styles.row}>
+                        <Image src="/images/vokabel-karten-hinzufügen/sprache-auswählen/italienisch.svg" className={styles.spracheAuswählenImg} alt="italienisch-card" id="italienisch-button" width={412} height={412} />
+                        <Image src="/images/vokabel-karten-hinzufügen/sprache-auswählen/spanisch.svg" className={styles.spracheAuswählenImg} alt="spanisch-card" id="spanisch-button" width={412} height={412} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- components/karteikarten-übersicht.tsx
+++ components/karteikarten-übersicht.tsx
@@ -34,7 +34,7 @@ export default function KarteikartenUebersicht() {
                 width={250}
                 height={250}
                 onClick={() =>
-                    router.push("/karteikarten-hinzufügen")
+                    router.push("/karteikarten-hinzufuegen")
                 }
             />
 
